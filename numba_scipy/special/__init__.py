baab24419a76c7768c1c64324e6ec002a5a9b812
Charis E Shiny <echaris.shiny@gmail.com>
2025-05-16 14:51:04 +0530

    Update __init__.py
    update for logging setup

--- numba_scipy/special/__init__.py
+++ numba_scipy/special/__init__.py
@@ -1,3 +1,17 @@
-from . import overloads as _overloads
+import logging
+
+logger = logging.getLogger("numba_scipy.special")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)  # Change to logging.DEBUG for more verbosity
+
+from . import overloads
 
 _overloads.add_overloads()
+
+def set_verbose_logging(enabled=True):
+    level = logging.DEBUG if enabled else logging.INFO
+    logger.setLevel(level)
+    logger.info(f"Verbose logging {'enabled' if enabled else 'disabled'}")
